cad69429bb1dc9d7524f42d6c9e611f7110f137a
maloryware <malory@onenetbeyond.org>
2025-02-27 08:25:05 +0000

    fixed duplicates

--- 02-shell01/ex07/r_dwssap.sh
+++
@@ -1,9 +0,0 @@
-# okay so, display 'cat /etc/passwd' with the following wincons:
-# 1 - remove comments
-# 2 - every other line starting from the second (sed -n "2~2p")
-# 3 - reverse each login
-# 4 - sort by reverse alphabetical order
-# 5 - keep only logins between FT_LINE1 and FT_LINE2 (including)
-# 6 - separate by ', '
-# 7 - end output with a '.'
-# have fun moron lol
